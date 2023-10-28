159358c310eada68b3550c13300cab5421b90df9
GDPlayer1035 <51692409+GDPlayer@users.noreply.github.com>
2023-10-28 10:59:40 +0800

    Update whay.js

--- whay.js
+++ whay.js
@@ -1,6 +1,10 @@
 // IMPOER T THIS ON PENGUINMOD
 class Whay {
   getInfo() {
+    const AllAsciiChars=' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+    const AllLettersUppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const AllLettersLowercase="abcdefghijklmnopqrstuvwxyz"
+    const AllSymbols=' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
     return {
       id: 'randomtruefalse',
       name: 'whay',
@@ -9,14 +13,45 @@ class Whay {
         {
           opcode: 'randomtruefalse',
           blockType: Scratch.BlockType.BOOLEAN,
-          text: 'whay'
+          text: 'random bool'
+        },
+        {
+          opcode: 'randomchar',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'random char of type: [TYPE]',
+          arguments: {
+            type: Scratch.ArgumentType.STRING,
+            menu: 'RANDOMTYPE'
+          }
         }
-      ]
+      ],
+      menus: {
+        RANDOMTYPE: {
+          acceptReporters: false,
+          items: ["ascii","uppercase","lowercase","symbols"]
+        }
+      }
     };
   }
 
   randomtruefalse() {
     return [false,true][Math.round(Math.random())]
+  }
+  randomchar(type){
+    switch(type){
+      case "ascii":
+        return AllAsciiChars[Math.round(Math.random()*AllAsciiChars.length)]
+        break;
+      case "uppercase":
+        return AllLettersUppercase[Math.round(Math.random()*AllLettersUppercase.length)]
+        break;
+      case "lowercase":
+        return AllLettersLowercase[Math.round(Math.random()*AllLettersLowercase.length)]
+        break;
+      case "symbols":
+        return AllSymbols[Math.round(Math.random()*AllSymbols.length)]
+        break;
+    }
   }
 }
 
